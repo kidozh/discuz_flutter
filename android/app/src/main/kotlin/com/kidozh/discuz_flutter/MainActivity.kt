f99c80e37201030acc828ed49b0898e944c67b7a
kidozh <331837926@qq.com>
2022-03-13 18:19:18 +0000

    migrate to newer flutter

--- android/app/src/main/kotlin/com/kidozh/discuz_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/kidozh/discuz_flutter/MainActivity.kt
@@ -1,6 +1,23 @@
 package com.kidozh.discuz_flutter
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine)
+    {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel (flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+        .setMethodCallHandler(
+        (call, result
+    ) -> {
+        // Your existing code
+    }
+        );
+    }
 }
