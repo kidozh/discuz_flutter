0458575ffc4da8ea3b7a3c368dfc1d76fe5c82c9
kidozh <331837926@qq.com>
2022-05-26 23:16:38 +0100

    add configuration & change protocol

---
+++ lib/page/SelectSignatureStylePage.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class SelectSignatureStylePage extends StatefulWidget{
+  @override
+  SelectSignatureStyleState createState() {
+    return SelectSignatureStyleState();
+  }
+
+}
+
+class SelectSignatureStyleState extends State<SelectSignatureStylePage>{
+  @override
+  Widget build(BuildContext context) {
+    return PlatformScaffold(
+
+    );
+  }
+
+
+}
