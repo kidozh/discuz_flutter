60f5e320e3359cb0ff75c9cd775ed45cb0d730e9
kidozh <kidozh@gmail.com>
2022-06-13 01:32:31 +0100

    update ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -11,6 +11,7 @@ import Flutter
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
     }
+    application.registerForRemoteNotifications()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
