571e12d725356ea4a3eb65e00ab19a4ed3df733d
kidozh <331837926@qq.com>
2022-11-28 00:01:25 +0000

    update ad

--- lib/utility/AdHelper.dart
+++ lib/utility/AdHelper.dart
@@ -19,7 +19,7 @@ class AdHelper {
     if (Platform.isAndroid) {
       return 'ca-app-pub-3940256099942544/2247696110';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/3986624511';
+      return 'ca-app-pub-4589701606972085/6863168152';
     }
     return 'ca-app-pub-3940256099942544/2247696110';
   }
