edc6a8650e5a56acd32415bbe371602da4d40f55
kidozh <331837926@qq.com>
2023-04-29 22:34:41 +0100

    allow haptic feedback

--- lib/utility/VibrationUtils.dart
+++ lib/utility/VibrationUtils.dart
@@ -1,38 +1,44 @@
+import 'package:discuz_flutter/utility/UserPreferencesUtils.dart';
 import 'package:flutter_vibrate/flutter_vibrate.dart';
 
 class VibrationUtils{
 
   static void vibrateWithClickIfPossible() async{
     bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate){
+    bool enableHapticFeedback = await UserPreferencesUtils.getHapticFeedbackPreference();
+    if (canVibrate && enableHapticFeedback){
       Vibrate.feedback(FeedbackType.medium);
     }
   }
 
   static void vibrateWithSwitchIfPossible() async{
     bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate){
+    bool enableHapticFeedback = await UserPreferencesUtils.getHapticFeedbackPreference();
+    if (canVibrate && enableHapticFeedback){
       Vibrate.feedback(FeedbackType.heavy);
     }
   }
 
   static void vibrateSuccessfullyIfPossible() async{
     bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate){
+    bool enableHapticFeedback = await UserPreferencesUtils.getHapticFeedbackPreference();
+    if (canVibrate && enableHapticFeedback){
       Vibrate.feedback(FeedbackType.success);
     }
   }
 
   static void vibrateWithSelectionIfPossible() async{
     bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate){
+    bool enableHapticFeedback = await UserPreferencesUtils.getHapticFeedbackPreference();
+    if (canVibrate && enableHapticFeedback){
       Vibrate.feedback(FeedbackType.selection);
     }
   }
 
   static void vibrateErrorIfPossible() async{
     bool canVibrate = await Vibrate.canVibrate;
-    if (canVibrate){
+    bool enableHapticFeedback = await UserPreferencesUtils.getHapticFeedbackPreference();
+    if (canVibrate && enableHapticFeedback){
       Vibrate.feedback(FeedbackType.error);
     }
   }
