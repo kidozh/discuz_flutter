149c42423fb33dd8e7561d7158301a00fb1c7b3d
g94840jz <jiduo.zhang@postgrad.manchester.ac.uk>
2024-02-08 14:08:47 +0000

    rendering update

--- lib/widget/DiscuzNotificationWidget.dart
+++ lib/widget/DiscuzNotificationWidget.dart
@@ -7,6 +7,7 @@ import 'package:discuz_flutter/utility/TimeDisplayUtils.dart';
 import 'package:discuz_flutter/utility/URLUtils.dart';
 import 'package:discuz_flutter/widget/DiscuzHtmlWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
 // ignore: must_be_immutable
 class DiscuzNotificationWidget extends StatelessWidget {
@@ -19,80 +20,101 @@ class DiscuzNotificationWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Brightness brightness = MediaQuery.of(context).platformBrightness;
-    return Padding(
-        padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
-        child: Card(
-          elevation: 4.0,
-          color: brightness == Brightness.light? Colors.white: Colors.black45,
-          surfaceTintColor: brightness == Brightness.light? Colors.white: Colors.black45,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
+    return PlatformWidgetBuilder(
+      cupertino: (context, child, platform){
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if(child!=null)
+            child!,
+            Divider(),
+          ],
+        );
+      },
+      material: (context, child, platform){
+        return Card(
+          elevation: 6.0,
+          // color: brightness == Brightness.light? Colors.white: Colors.black45,
+          // surfaceTintColor: brightness == Brightness.light? Colors.white: Colors.black45,
+          child: Padding(
+            padding: EdgeInsets.all(4.0),
+            child: child,
+          ),
+        );
+      },
+      child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: CachedNetworkImage(
-                  imageUrl: URLUtils.getAvatarURL(_discuz, _notification.authorId.toString()),
-                  progressIndicatorBuilder: (context, url, downloadProgress) => CircularProgressIndicator(value: downloadProgress.progress),
-                  errorWidget: (context, url, error) => Container(
-                    width: 16.0,
-                    height: 16.0,
-                    child: CircleAvatar(
-                      backgroundColor: CustomizeColor.getColorBackgroundById(_notification.uid),
-                      child: Text(
-                        _notification.author.length != 0
-                            ? _notification.author[0].toUpperCase()
-                            : S.of(context).notification[0].toUpperCase(),
-                        style: TextStyle(color: Colors.white,fontSize: 12),
-                      ),
-                    ),
-                  ),
-                  imageBuilder: (context, imageProvider) => Container(
-                    width: 16.0,
-                    height: 16.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: imageProvider, fit: BoxFit.cover),
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                  child: Row(
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text: "",
-                          style: DefaultTextStyle.of(context).style,
-                          children: <TextSpan>[
-                            if(_notification.author.isNotEmpty)
-                              TextSpan(text: _notification.author, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.deepOrange.shade400, fontSize: 12)),
-                            if(_notification.author.isEmpty)
-                              TextSpan(text: _notification.type, style: TextStyle(fontWeight: FontWeight.bold, color: Colors.indigo.shade400, fontSize: 12)),
-                            TextSpan(text: ' · '),
-                            TextSpan(text: TimeDisplayUtils.getLocaledTimeDisplay(context,_notification.dateline), style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12)),
-
-                          ],
+              Row(
+                children: [
+                  if(_notification.author.isNotEmpty)
+                  Padding(
+                    padding: EdgeInsets.only(right: 8.0),
+                    child: CachedNetworkImage(
+                      imageUrl: URLUtils.getAvatarURL(_discuz, _notification.authorId.toString()),
+                      progressIndicatorBuilder: (context, url, downloadProgress) => CircularProgressIndicator(value: downloadProgress.progress),
+                      errorWidget: (context, url, error) => Container(
+                        width: 16.0,
+                        height: 16.0,
+                        child: CircleAvatar(
+                          backgroundColor: CustomizeColor.getColorBackgroundById(_notification.uid),
+                          child: Text(
+                            _notification.author.length != 0
+                                ? _notification.author[0].toUpperCase()
+                                : S.of(context).notification[0].toUpperCase(),
+                            style: TextStyle(color: Colors.white,fontSize: 12),
+                          ),
                         ),
                       ),
-                      Spacer(),
-                      Padding(
-                        padding: EdgeInsets.only(right: 8.0),
-                        child: Text(_notification.type,style: TextStyle(fontWeight: FontWeight.w400, fontSize: 12),),
-                      )
-                      ,
+                      imageBuilder: (context, imageProvider) => Container(
+                        width: 24.0,
+                        height: 24.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: imageProvider, fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                      child: Row(
+                        children: [
+                          //SizedBox(width: 8.0,),
+                          RichText(
+                            text: TextSpan(
+                              text: "",
+                              style: Theme.of(context).textTheme.labelLarge,
+                              children: <TextSpan>[
+                                if(_notification.author.isNotEmpty)
+                                  TextSpan(text: _notification.author, style: TextStyle(fontWeight: FontWeight.bold)),
+                                if(_notification.author.isEmpty)
+                                  TextSpan(text: _notification.type.toUpperCase(), style: TextStyle(fontWeight: FontWeight.bold, color: Theme.of(context).colorScheme.primary)),
 
-                    ],
-                )
-              )
+                              ],
+                            ),
+                          ),
+                          Spacer(),
+                          Padding(
+                              padding: EdgeInsets.only(right: 8.0),
+                              child: Text(TimeDisplayUtils.getLocaledTimeDisplay(context,_notification.dateline),
+                                style: Theme.of(context).textTheme.labelLarge,
+                              ),
+                            ),
+                        ],
+                      )
+                  )
+                ],
+              ),
+              SizedBox(height: 8.0,),
+              // rich text rendering
+              DiscuzHtmlWidget(_discuz,_notification.note, onSelectTid: this.onSelectTid,)
             ],
-          ),
-          // rich text rendering
-          DiscuzHtmlWidget(_discuz,_notification.note, onSelectTid: this.onSelectTid,)
-        ],
+          )
       ),
-    ));
+    );
   }
 }
