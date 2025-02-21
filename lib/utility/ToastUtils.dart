8003b0cf0c95811404a002697be4d28c908f112e
kidozh <11661760+kidozh@users.noreply.github.com>
2025-02-21 17:52:55 +0000

    fix android issue

--- lib/utility/ToastUtils.dart
+++ lib/utility/ToastUtils.dart
@@ -3,7 +3,6 @@
 import 'package:discuz_flutter/utility/AppPlatformIcons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-import 'package:toastification/toastification.dart';
 
 class ToastUtils{
   static Future<void> showSuccessfulToast(String msg) {
@@ -14,30 +13,30 @@ class ToastUtils{
 
   }
 
-  static void showInfoToast(BuildContext context, String msg) {
-    // EasyLoading.showSuccess(
-    //   msg,
-    // );
+  // static void showInfoToast(BuildContext context, String msg) {
+  //   // EasyLoading.showSuccess(
+  //   //   msg,
+  //   // );
+  //
+  //   toastification.show(context: context,
+  //       title: Text(msg),
+  //       style: ToastificationStyle.fillColored,
+  //       autoCloseDuration: const Duration(seconds: 2),
+  //       showProgressBar: false,
+  //       type: ToastificationType.info,
+  //       icon: Icon(AppPlatformIcons(context).check),
+  //       alignment: Alignment.topCenter
+  //
+  //
+  //   );
+  // }
 
-    toastification.show(context: context,
-        title: Text(msg),
-        style: ToastificationStyle.fillColored,
-        autoCloseDuration: const Duration(seconds: 2),
-        showProgressBar: false,
-        type: ToastificationType.info,
-        icon: Icon(AppPlatformIcons(context).check),
-        alignment: Alignment.topCenter
-
-
-    );
-  }
-
-  static void showErrorToast(BuildContext context, String msg) {
-    toastification.show(context: context,
-      title: Text(msg),
-      style: ToastificationStyle.fillColored,
-      autoCloseDuration: const Duration(milliseconds: 500),
-      type: ToastificationType.error
-    );
-  }
+  // static void showErrorToast(BuildContext context, String msg) {
+  //   toastification.show(context: context,
+  //     title: Text(msg),
+  //     style: ToastificationStyle.fillColored,
+  //     autoCloseDuration: const Duration(milliseconds: 500),
+  //     type: ToastificationType.error
+  //   );
+  // }
 }
